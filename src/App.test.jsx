74793d863f62cbee2cf886bafc605f0fdea308c8
BrendenSeifried <brendenseifried@gmail.com>
2022-05-23 12:21:39 -0700

    added memory router test passes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 describe('test for CI', () => {
   it('testing for CI', async () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
   });
 });
